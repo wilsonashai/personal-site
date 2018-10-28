6b8a3f4696b5e0ba6e9b58d569321bd7f557894f
Asha Wilson <awilson@Ashas-MacBook-Pro.local>
2018-10-27 21:04:21 -0400

    recent changes including temp sidebar before tinker

--- src/App.js
+++ src/App.js
@@ -2,17 +2,23 @@ import React, { Component } from 'react';
 import './styles/App.css';
 import { Grid } from 'semantic-ui-react';
 import NavBar from './components/NavBar';
+import SideBar from './components/SideBar';
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <NavBar/>
-                <Grid>
-                    <Grid.Row>
-                        <p className="App-intro">Endless possibilities await...</p>
-                    </Grid.Row>
-                </Grid>
+                {/* <NavBar/> */}
+                <div className="Content">
+                    <Grid columns={16}>
+                        <Grid.Row>
+                            <Grid.Column width={4}>
+                                <SideBar/>
+                            </Grid.Column>
+                            <Grid.Column width={12}/>
+                        </Grid.Row>
+                    </Grid>
+                </div>
             </div>
         );
     }
---
+++ src/components/SideBar.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { Grid, Header, Icon } from 'semantic-ui-react';
+
+class SideBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div className="side-bar-container">
+                <div className="top-header">
+                    <h1 className="name-header">Asha Wilson</h1>
+                    <p className="subheader">Developer | Designer</p>
+                </div>
+                <div className="bottom-header">
+                    <Grid columns={4}>
+                        <Grid.Row className="sidebar-links">
+                            <a href="mailto:wilson.ashai@gmail.com"><Icon size="large" name="mail"/> wilson.ashai@gmail.com</a>
+                        </Grid.Row>
+                        <Grid.Row className="sidebar-links">
+                            <a href="https://github.com/wilsonashai" target="_blank"><Icon size="large" name="github"/> github/wilsonashai</a>
+                        </Grid.Row>
+                        <Grid.Row className="sidebar-links">
+                            <a href="https://www.linkedin.com/in/asha-wilson-5618b483" target="_blank"><Icon size="large" name="linkedin"/> Asha Wilson</a>
+                        </Grid.Row>
+                        <Grid.Row className="sidebar-links">
+                        <a href="https://www.goodreads.com/eightqueens" target="_blank"><Icon size="large" name="goodreads"/> goodreads/eightqueens</a>
+                        </Grid.Row>
+                    </Grid>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default SideBar;
