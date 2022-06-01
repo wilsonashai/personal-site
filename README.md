e3797b62aaa59ba29a7902b0115478677402db76
Asha W <wilson.ashai@gmail.com>
2022-05-31 21:10:16 -0400

    this thing is still kicking huh?

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 ## Asha Wilson's personal site.
 
+[![Netlify Status](https://api.netlify.com/api/v1/badges/f0d197b4-460b-4443-bcbc-1ea03841f4dd/deploy-status)](https://app.netlify.com/sites/asha-personal-site/deploys)
+
 This is my personal site to display my personal and professional background to those interested.
 It was designed by myself using React and Semantic-UI and hosted on Netlify.
 
-To view, please head to https://www.ashawilson.com/.
+To view, please head to https://www.ashawilson.netlify.app/.
--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Container, Image } from 'semantic-ui-react';
-import paris_img from './../images/paris_square.jpg';
+import updated_headshot from './../images/cropped_linkedin_headshot.jpeg';
 
 class About extends Component {
     constructor(props) {
@@ -13,29 +13,28 @@ class About extends Component {
             <Container fluid textAlign='left'>
                 <div className="title">
                     <div className="major-header">Hello! I'm Asha Wilson&mdash;</div>
-                    <div className="subheader">a software engineer and designer based in Boston.</div>
+                    <div className="subheader">a software engineer based in Washington DC.</div>
                 </div>
                 <br/>
                 <div className="about-container">
                     <div className="about-text">
-                        <p className="blurb">Currently, I work as a Software Engineer at Liberty Mutual in their Digital Sales
-                        group supporting the US renters and auto quoting experiences. I joined Liberty after graduating from the University of Pittsburgh's 
+                        <p className="blurb">Currently, I work as a Software Engineer II at Micrososft supporting commercial self-service checkout
+                        at Microsoft in their Commerce, Platform + Experience group. 
+                        <br/>
+                        <br/>
+                        Previously, I worked at Liberty Mutual in their Digital Sales group supporting the US renters and auto 
+                        quoting experiences. I joined Liberty after graduating from the University of Pittsburgh's 
                         School of Computing and Information in May 2016 with a BS in Information Science.
-                        {/* <p className="blurb">Recently, 
-                            I was nominated and sponsored by Liberty Mutual to participate in The Partnership Inc's <a className="info-link" href=
-                            "https://www.thepartnershipinc.org/services/leadership-development/" target="_blank" rel="noopener noreferrer">
-                            Associates Leadership Development Program</a> for their 2019 class.
-                        </p> */}
                         <br/>
                         <br/>
-                        Outside of work, I am the Events and Volunteer Lead for the Boston chapter of <a className="info-link" href="http://www.blackgirlscode.com/" 
-                        target="_blank" rel="noopener noreferrer">Black Girls Code</a> and serve as a mentor for <a className="info-link" href="https://hackdiversity.com/" 
-                        target="_blank" rel="noopener noreferrer">Hack.Diversity</a>. Besides technology and design, some of my other interests include keeping up with the 
-                        NBA, trying out new nail art designs, and reading a good deal of books.
+                        Outside of work, I served as the Events and Volunteer Lead for the Boston chapter of <a className="info-link" href="http://www.blackgirlscode.com/" 
+                        target="_blank" rel="noopener noreferrer">Black Girls Code</a> for 3 years and serve as a mentor for <a className="info-link" href="https://hackdiversity.com/" 
+                        target="_blank" rel="noopener noreferrer">Hack.Diversity</a>. Besides technology and design, some of my other interests and hobbies include trying out new nail art designs, 
+                        ice skating, traveling and reading lots of books.
                         </p>
                     </div>
                     <div className="about-image">
-                        <Image src={paris_img} fluid circular/>
+                        <Image src={updated_headshot} fluid circular/>
                     </div>
                 </div>
             </Container>
